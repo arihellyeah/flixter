d6160893f631a09f6b19822bdd38752f4b2f58ce
Arielle <arieloring@gmail.com>
2019-09-16 00:02:41 +0000

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [:update]
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] do
-      end
+      resources :sections, only: [:new, :create]
     end
   end
 end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
